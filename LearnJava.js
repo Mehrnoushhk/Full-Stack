5a26f2a147255ae67e9d16becdf373b987ef25a9
Mehrnoush <mehrnoosh.haghighat@gmail.com>
2020-10-20 13:10:49 +0330

    commited 1020-1

---
+++ LearnJava.js
@@ -0,0 +1,19 @@
+'use strict';
+let day= 5;
+switch (day){
+  case 1:
+    console.log("It is Monday");
+  case 2:
+    console.log("It is Tuesday");
+  case 3:
+    console.log("It is Wensday");
+  case 4:
+    console.log("It is Thursday");
+  case 5:
+    console.log("It is Friday");
+  case 6:
+    console.log("It is Saturday");
+  case 7:
+    console.log("It is Sunday");
+
+}
